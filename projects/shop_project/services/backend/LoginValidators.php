e3a60d8abf72db38c256aee6e56df5a7ce796cab
Duong Khanh Duy <102469987+besduy04@users.noreply.github.com>
2024-01-24 13:20:53 +0700

    update LoginValidators.php

--- projects/shop_project/services/backend/LoginValidators.php
+++ projects/shop_project/services/backend/LoginValidators.php
@@ -14,7 +14,32 @@ class LoginValidators
     public function validate(): bool
     {
         // check cac case validation
+        if (!$this->validateUsername() || !$this->validatePassword()) {
+            return false;
+        }
 
+        return true;
+    }
+
+    public function validateUsername(): bool
+    {
+        // Check length & empty
+        if (empty($this->username) || !((strlen($this->username) > 8) && (strlen($this->username) < 15))) {
+            return false;
+        }
+        return true;
+    }
+
+    public function validatePassword(): bool
+    {
+        // Check length & empty
+        if (empty($this->password) || (strlen($this->password) < 8)) {
+            return false;
+        }
+        // Check password's strength
+        if (!preg_match("/[^A-Za-z0-9 -]+/", $this->password)) {
+            return false;
+        }
         return true;
     }
 }
