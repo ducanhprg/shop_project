88cc625c4114ad58d512c2ed4b6ac058f8421f60
TheMiracleBanana <miraclebanana277@gmail.com>
2024-01-24 22:57:31 +0700

    update LoginValidation.php file with check empty, check length, check special character

--- projects/shop_project/services/backend/LoginValidators.php
+++ projects/shop_project/services/backend/LoginValidators.php
@@ -10,10 +10,30 @@ class LoginValidators
         $this->username = $username;
         $this->password = $password;
     }
+    private function specialChar($username): bool|int
+    {
+        return preg_match('/[^a-zA-Z0-9]/', $username);
+    }
 
     public function validate(): bool
     {
         // check cac case validation
+        // Checks if username or password empty
+        if (empty($this->username) || empty($this->password)) {
+            return false;
+        }
+
+        // Check if username too long
+        if (strlen($this->username) > 50) {
+            return false;
+        }
+
+        // Check special characters
+        if ($this->specialChar($this->username)){
+            return false;
+        }
+
+        // Additional checks
 
         return true;
     }
