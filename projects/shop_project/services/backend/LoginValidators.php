cbcdd6a5034ddb926e1eb5a46f7efe3c9672d3c2
Taidung <taidung3010@gmail.com>
2024-01-25 22:02:42 +0700

    Update LoginValidators.php
    Them cac case validator

--- projects/shop_project/services/backend/LoginValidators.php
+++ projects/shop_project/services/backend/LoginValidators.php
@@ -14,7 +14,20 @@ class LoginValidators
     public function validate(): bool
     {
         // check cac case validation
+        // Check empty username & password
+        if (empty($this->username) || empty($this->password)) {
+            return false;
+        }
 
+        // Check length username & password
+        if (strlen($this->username) < 3|| strlen($this->password) < 5) {
+            return false;
+        }
+
+        // Check password contain letter and number
+        if (!preg_match("/[a-zA-Z]/", $this->password) || !preg_match("/[0-9]/", $this->password)) {
+            return false;
+        }
         return true;
     }
 }
